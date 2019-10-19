788421a4fea7bcd8c93479c4d515a33c46718c61
Praveen Nishanth <praveennishanth@gmail.com>
2019-10-19 13:51:37 +0530

    reverse String

--- reverseString-Praveen.js
+++ reverseString-Praveen.js
@@ -1,1 +1,16 @@
-//sasas
+function reverse(string) {
+    if (!string || string.length < 2 ||
+        typeof string !== 'string') {
+        return 'Not valid';
+    }
+    var strArr = [];
+    var len = string.length - 1;
+    for (let i = len; i >= 0; i--) {
+        strArr.push(string[i]);
+    }
+    return strArr.join('');
+}
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("hello");
