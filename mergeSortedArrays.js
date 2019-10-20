0ba76752133f27d2d0cf5ba9211b79fec1df5801
Karine <karina.a182@gmail.com>
2019-10-20 22:23:47 +1100

    added sorted arrays method

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,11 +1,17 @@
-function mergeSortedArrays(arr1, arr2) {
-    const newArray = [];
-    for (let i = 0; i < arr2.length; i++) {
-        if (arr1[i] === arr2[i]) {
-            return;
-        }
-        arr1.push(arr2[i])
+let filledArray = new Array(100).fill(5);
+
+(function mergeSortedArrays(arr1, arr2) {
+    if (arr1 && arr2 && !arr1.some(isNaN) && !arr2.some(isNaN)) {
+        arr2.map((elm) => {
+            if (!arr1.includes(elm)) {
+                arr1.push(elm)
+            }
+        })
+        console.log(arr1.sort());
+        return arr1.sort();
+
+    } else {
+        console.log("input invalid");
     }
-    console.log(arr1.sort())
-}
-mergeSortedArrays([6, 7], [1, 6, 7, 8, 9])
+
+}([9, 5, 3, 999, 4], [2, 3]));
